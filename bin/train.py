49115572910b1c305379187d1d5329ef34588829
myao1 <myao1135@gmail.com>
2020-12-29 13:50:45 +0000

    Add function to save experiment results

--- bin/train.py
+++ bin/train.py
@@ -1,6 +1,8 @@
 import time
 import os
+import matplotlib.pyplot as plt
 
+import torch
 from torch import optim, nn
 
 from data.dataloader import create_data_loaders
@@ -38,6 +40,40 @@ def train_model(train_loader, val_loader, my_model, optimizer, num_epochs, crite
     return train_loss, train_accuracy, val_loss, val_accuracy
 
 
+def save_model(my_model, path_name):
+    print('Saving model...')
+    torch.save(my_model.state_dict(), os.path.join(config["output_path"], path_name))
+
+
+# TODO: edit path name - refactor, put in different folder - utils?
+def make_plots(train_loss, train_accuracy, val_loss, val_accuracy, path_name):
+
+    #with open('C:/path/numbers.txt') as f:
+    #lines = f.read().splitlines()
+    plt.figure(figsize=(10, 7))
+    plt.plot(train_accuracy, color='green', label='train accuracy')
+    plt.plot(val_accuracy, color='blue', label='validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(config["output_path"], path_name))
+
+    plt.figure(figsize=(10, 7))
+    plt.plot(train_loss, color='orange', label='train loss')
+    plt.plot(val_loss, color='red', label='validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join(config["output_path"], path_name))
+
+
+def write_list_to_file(my_list, file_name, path_name="../outputs/raw_data/"):
+    file_name = os.path.join(path_name, file_name)
+    with open(file_name, 'w') as f:
+        for item in my_list:
+            f.write("%s\n" % item)
+
+
 def main():
     criterion = nn.BCEWithLogitsLoss()
     model_fine_tune_added_layers = model(pretrained=True, requires_grad=True, add_layers=True).cuda()
@@ -51,6 +87,11 @@ def main():
                                                                      num_epochs=config["training"]["num_epochs"],
                                                                      criterion=criterion)
     # TODO: output graphs? - find some sort of tracker
+    write_list_to_file(train_loss, "trainloss1.txt")
+    write_list_to_file(train_accuracy, "trainacc1.txt")
+    write_list_to_file(val_loss, "valloss1.txt")
+    write_list_to_file(val_accuracy, "valaccuracy1.txt")
+
     return train_loss, train_accuracy, val_loss, val_accuracy
 
 
