72d236529107e1a47a3397b6ed54131b5680fba1
a-r-j <arjamasb@gmail.com>
2021-01-02 19:56:21 +0000

    [Training] refactor training setup

--- bin/train.py
+++ bin/train.py
@@ -1,102 +1,91 @@
-import time
+import argparse
+import logging
 import os
-import matplotlib.pyplot as plt
+import time
 
 import torch
-from torch import optim, nn
+import yaml
+from torch import nn, optim
 
 from data.dataloader import create_data_loaders
 from model.base import model
-from training.trainer import validate, fit
+from training.trainer import Trainer
+from utils.utils import save_model, write_list_to_file
 
-import yaml
+log = logging.getLogger(__name__)
 
 CONFIG_PATH = "../configs/"
 
 
-def load_config(config_name):
+def load_config(config_name: str):
     with open(os.path.join(CONFIG_PATH, config_name)) as file:
         my_config = yaml.safe_load(file)
     return my_config
 
 
-config = load_config("config.yaml")
-
-
-def train_model(train_loader, val_loader, my_model, optimizer, num_epochs, criterion):
-    train_loss, train_accuracy = [], []
-    val_loss, val_accuracy = [], []
-    start = time.time()
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        train_epoch_loss, train_epoch_accuracy = fit(my_model, train_loader, optimizer, criterion)
-        val_epoch_loss, val_epoch_accuracy = validate(my_model, val_loader, criterion)
-        train_loss.append(train_epoch_loss)
-        train_accuracy.append(train_epoch_accuracy)
-        val_loss.append(val_epoch_loss)
-        val_accuracy.append(val_epoch_accuracy)
-    end = time.time()
-    print(f"{(end - start) / 60:.3f} minutes")
-    return train_loss, train_accuracy, val_loss, val_accuracy
-
-
-def save_model(my_model, path_name):
-    print('Saving model...')
-    torch.save(my_model.state_dict(), os.path.join(config["output_path"], path_name))
-
-
-# TODO: edit path name - refactor, put in different folder - utils?
-def make_plots(train_loss, train_accuracy, val_loss, val_accuracy, path_name):
-
-    #with open('C:/path/numbers.txt') as f:
-    #lines = f.read().splitlines()
-    plt.figure(figsize=(10, 7))
-    plt.plot(train_accuracy, color='green', label='train accuracy')
-    plt.plot(val_accuracy, color='blue', label='validation accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.savefig(os.path.join(config["output_path"], path_name))
-
-    plt.figure(figsize=(10, 7))
-    plt.plot(train_loss, color='orange', label='train loss')
-    plt.plot(val_loss, color='red', label='validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig(os.path.join(config["output_path"], path_name))
-
-
-def write_list_to_file(my_list, file_name, path_name="../outputs/raw_data/"):
-    file_name = os.path.join(path_name, file_name)
-    with open(file_name, 'w') as f:
-        for item in my_list:
-            f.write("%s\n" % item)
+# Todo check config parsing from command line. You hould be able to launch a run with: python bin/train.py -c $PATH_TO_CONFIG
+# Todo use typehinting
+# Todo setup logging
 
 
 def main():
-    criterion = nn.BCEWithLogitsLoss()
-    model_fine_tune_added_layers = model(pretrained=True, requires_grad=True, add_layers=True).cuda()
-    optimizer_fine_tune_added_layers = optim.SGD(model_fine_tune_added_layers.parameters(),
-                                                 lr=config["training"]["optimizer"]["learning_rate"],
-                                                 momentum=config["training"]["optimizer"]["momentum"])
-    train_loader, val_loader, test_loader = create_data_loaders(config["dataset"]["csv_name"])
-    train_loss, train_accuracy, val_loss, val_accuracy = train_model(train_loader, val_loader,
-                                                                     model_fine_tune_added_layers,
-                                                                     optimizer=optimizer_fine_tune_added_layers,
-                                                                     num_epochs=config["training"]["num_epochs"],
-                                                                     criterion=criterion)
-    # TODO: output graphs? - find some sort of tracker
-    write_list_to_file(train_loss, "trainloss1.txt")
-    write_list_to_file(train_accuracy, "trainacc1.txt")
-    write_list_to_file(val_loss, "valloss1.txt")
-    write_list_to_file(val_accuracy, "valaccuracy1.txt")
+    # Create Data
+    train_loader, val_loader, test_loader = create_data_loaders(
+        config["dataset"]["csv_name"]
+    )
 
+    # Create Model
+    # Todo model params should be configurable from config
+    model_fine_tune_added_layers = model(
+        pretrained=True, requires_grad=True, add_layers=True
+    ).cuda()
+
+    # Create loss
+    # Todo this should be configurable from config
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Create optimizer
+    # Todo optimizer type should be configurable from config (ADAM, etc)
+    optimizer_fine_tune_added_layers = optim.SGD(
+        model_fine_tune_added_layers.parameters(),
+        lr=config["training"]["optimizer"]["learning_rate"],
+        momentum=config["training"]["optimizer"]["momentum"],
+    )
+
+    # Create Trainer
+    trainer = Trainer(
+        model=model_fine_tune_added_layers,
+        optimizer=optimizer_fine_tune_added_layers,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        test_loader=test_loader,
+        criterion=criterion,
+        config=config["training"],
+    )
+
+    # Write logs
+    # TODO: output graphs? - find some sort of tracker
+    trainer.write_logs_to_file()
+
+    trainer.save_model("models/model2000.pth")
     save_model(model_fine_tune_added_layers, "models/model2000.pth")
 
     return train_loss, train_accuracy, val_loss, val_accuracy
 
 
 if __name__ == "__main__":
-    main()
+    # Parse command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        default="/Users/arianjamasb/github/Part2ProjectUpdated/config/default_config.json",
+        help="Path to project config file",
+        required=False,
+    )
+    args = parser.parse_args()
 
+    parser.parse_args()
+    config = load_config("config.yaml")
+
+    main()
--- training/trainer.py
+++ training/trainer.py
@@ -1,57 +1,125 @@
+from __future__ import annotations
+
+import logging
+
 import torch
 from tqdm import tqdm
 
+from utils.utils import save_model, write_list_to_file
 
-def validate(model, val_loader, criterion):
-    print('Validating')
-    model.eval()
-    val_running_loss = 0.0
-    val_running_correct = 0
-    dataset_length = len(val_loader.dataset)
-    with torch.no_grad():
-        for i, data in tqdm(enumerate(val_loader), total=int(dataset_length / val_loader.batch_size)):
+log = logging.getLogger(__name__)
+
+# Todo type hinting, logging
+# Todo add commentary to training funcs
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module,
+        optimizer,
+        train_loader: Dataloader,
+        val_loader: Dataloader,
+        test_loader: Dataloader,
+        criterion,
+    ) -> None:
+        # Model
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+
+        # Data
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+
+        self.num_epoch = config["num_epochs"]
+
+        # Metrics
+        self.train_loss, self.train_accuracy = [], []
+        self.val_loss, self.val_accuracy = [], []
+
+    def train_model(self) -> None:
+        start = time.time()
+        for epoch in range(self.num_epochs):
+            print("Epoch {}/{}".format(epoch, self.num_epochs - 1))
+
+            # Perform training and validation iterations
+            train_epoch_loss, train_epoch_accuracy = self.train_iteration()
+            val_epoch_loss, val_epoch_accuracy = self.validate()
+
+            # Log metrics
+            self.train_loss.append(train_epoch_loss)
+            self.train_accuracy.append(train_epoch_accuracy)
+            self.val_loss.append(val_epoch_loss)
+            self.val_accuracy.append(val_epoch_accuracy)
+        end = time.time()
+        print(f"{(end - start) / 60:.3f} minutes")
+
+    def train_iteration(self) -> Tuple[float, float]:
+        print("Training")
+        self.model.train()
+        train_running_loss = 0.0
+        train_running_correct = 0
+
+        dataset_length = len(self.train_loader.dataset)
+        for i, data in tqdm(
+            enumerate(self.train_loader),
+            total=int(dataset_length / self.train_loader.batch_size),
+        ):
+            # if i == 0:
+            #     print(dataset_length)
             data, target = data[0].cuda(), data[1].cuda()
-            outputs = model(data)
+            self.optimizer.zero_grad()
+            outputs = self.model(data)
             y = torch.zeros(list(outputs.size())[0], 2)
             y[range(y.shape[0]), target] = 1
             y = y.cuda()
-            loss = criterion(outputs, y)
-            val_running_loss += loss.item()
+            loss = self.criterion(outputs, y)
+            train_running_loss += loss.item()
             _, preds = torch.max(outputs.data, 1)
-            val_running_correct += (preds == target).sum().item()
+            train_running_correct += (preds == target).sum().item()
+            loss.backward()
+            self.optimizer.step()
 
-        val_loss = val_running_loss / dataset_length
-        val_accuracy = 100. * val_running_correct / dataset_length
-        print(f'Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.2f}')
+        train_loss = train_running_loss / dataset_length
+        train_accuracy = 100.0 * train_running_correct / dataset_length
+        print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_accuracy:.2f}")
 
-        return val_loss, val_accuracy
+        return train_loss, train_accuracy
 
+    def validate(self) -> Tuple[float, float]:
+        print("Validating")
+        self.model.eval()
+        val_running_loss = 0.0
+        val_running_correct = 0
+        dataset_length = len(self.val_loader.dataset)
+        with torch.no_grad():
+            for i, data in tqdm(
+                enumerate(self.val_loader),
+                total=int(dataset_length / self.val_loader.batch_size),
+            ):
+                data, target = data[0].cuda(), data[1].cuda()
+                outputs = self.model(data)
+                y = torch.zeros(list(outputs.size())[0], 2)
+                y[range(y.shape[0]), target] = 1
+                y = y.cuda()
+                loss = self.criterion(outputs, y)
+                val_running_loss += loss.item()
+                _, preds = torch.max(outputs.data, 1)
+                val_running_correct += (preds == target).sum().item()
 
-def fit(model, train_loader, optimizer, criterion):
-    print('Training')
-    model.train()
-    train_running_loss = 0.0
-    train_running_correct = 0
-    dataset_length = len(train_loader.dataset)
-    for i, data in tqdm(enumerate(train_loader), total=int(dataset_length / train_loader.batch_size)):
-        # if i == 0:
-        #     print(dataset_length)
-        data, target = data[0].cuda(), data[1].cuda()
-        optimizer.zero_grad()
-        outputs = model(data)
-        y = torch.zeros(list(outputs.size())[0], 2)
-        y[range(y.shape[0]), target] = 1
-        y = y.cuda()
-        loss = criterion(outputs, y)
-        train_running_loss += loss.item()
-        _, preds = torch.max(outputs.data, 1)
-        train_running_correct += (preds == target).sum().item()
-        loss.backward()
-        optimizer.step()
+            val_loss = val_running_loss / dataset_length
+            val_accuracy = 100.0 * val_running_correct / dataset_length
+            print(f"Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.2f}")
 
-    train_loss = train_running_loss / dataset_length
-    train_accuracy = 100. * train_running_correct / dataset_length
+            return val_loss, val_accuracy
 
-    print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_accuracy:.2f}")
+    def write_logs_to_file(self) -> None:
+        write_list_to_file(self.train_loss, "trainloss1.txt")
+        write_list_to_file(self.train_accuracy, "trainacc1.txt")
+        write_list_to_file(self.val_loss, "valloss1.txt")
+        write_list_to_file(self.val_accuracy, "valaccuracy1.txt")
 
-    return train_loss, train_accuracy
+    def save_model_to_file(self, filename: str) -> None:
+        save_model(self.model, filename)
