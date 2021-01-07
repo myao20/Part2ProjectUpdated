e714aa1f755a5d206cc61499e38bae37014625c2
myao1 <myao1135@gmail.com>
2021-01-07 16:37:49 +0000

    Add logging

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,5 +1,6 @@
 import os
 from typing import Tuple
+import logging
 
 import yaml
 from torch.utils.data import DataLoader
@@ -8,6 +9,13 @@ from data.create_dataset import split_data
 from data.dataset import DRDataset
 
 CONFIG_PATH = "../configs/"
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+fh = logging.FileHandler('../logs/data_size.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+log.addHandler(fh)
 
 
 def load_config(config_name):
@@ -21,6 +29,9 @@ config = load_config("config.yaml")
 
 def create_data_loaders(csv_name: str) -> Tuple[DataLoader, DataLoader, DataLoader]:
     x_train, y_train, x_val, y_val, x_test, y_test = split_data(csv_name)
+    log.debug(len(x_train))
+    log.debug(len(x_val))
+    log.debug(len(x_test))
     train_data = DRDataset(x_train, y_train, tfms=1)
     val_data = DRDataset(x_val, y_val, tfms=0)
     test_data = DRDataset(x_test, y_test, tfms=0)
