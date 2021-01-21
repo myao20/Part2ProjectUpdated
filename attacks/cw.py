8f7c690cbe64a31f275e87877b4b15ab09da4128
myao1 <myao1135@gmail.com>
2021-01-21 15:16:23 +0000

    Initial commit

---
+++ attacks/cw.py
@@ -0,0 +1,62 @@
+import logging
+import torch
+from torch import nn
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+fh = logging.FileHandler('../logs/cw.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+log.addHandler(fh)
+log.addHandler(ch)
+
+
+def cw(model: nn.Module, images, labels, c=10.0, kappa=0, max_iter=1000, learning_rate=0.01):
+    images = images.clone().detach().cuda()
+    labels = labels.clone().detach().cuda()
+
+    # Define f-function
+    def f(x):
+        outputs = model(x)
+        one_hot_labels = torch.eye(len(outputs[0]))[labels].cuda()
+
+        i, _ = torch.max((1 - one_hot_labels) * outputs, dim=1)
+        j = torch.masked_select(outputs, one_hot_labels.bool())
+
+        return torch.clamp(j - i, min=-kappa)
+
+    w = torch.zeros_like(images, requires_grad=True).cuda()
+
+    optimizer = torch.optim.Adam([w], lr=learning_rate)
+
+    prev = 1e10
+
+    for step in range(max_iter):
+        if step % 10 == 0:
+            log.info(f'Step {step} out of {max_iter}')
+        a = 1 / 2 * (nn.Tanh()(w) + 1)
+
+        loss1 = nn.MSELoss(reduction='sum')(a, images)
+        loss2 = torch.sum(c * f(a))
+
+        cost = loss1 + loss2
+
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        # Stop early if the loss doesn't converge
+        if step % (max_iter // 10) == 0:
+            if cost > prev:
+                log.info('Early stop')
+                return a
+            prev = cost
+
+    attack_images = 1 / 2 * (nn.Tanh()(w) + 1)
+
+    return attack_images
