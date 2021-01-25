600c7cf3bfaa1693c4d361da488f6e3ff0a525c5
myao1 <myao1135@gmail.com>
2021-01-25 10:19:04 +0000

    Minor changes

--- attacks/cw.py
+++ attacks/cw.py
@@ -6,9 +6,9 @@ from torch import nn
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 fh = logging.FileHandler('../logs/cw.log')
-fh.setLevel(logging.DEBUG)
+fh.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
@@ -23,13 +23,15 @@ def cw(model: nn.Module, images, labels, c=10.0, kappa=0, max_iter=1000, learnin
     # Define f-function
     def f(x):
         outputs = model(x)
+        # tensor of one-hot labels
         one_hot_labels = torch.eye(len(outputs[0]))[labels].cuda()
-
         i, _ = torch.max((1 - one_hot_labels) * outputs, dim=1)
+        # masked_select returns a new 1-D tensor which indexes the input tensor according
+        # to the boolean mask (one_hot_labels.bool()) which is a BoolTensor.
         j = torch.masked_select(outputs, one_hot_labels.bool())
 
         return torch.clamp(j - i, min=-kappa)
-
+    # tensor of zeroes same size as images
     w = torch.zeros_like(images, requires_grad=True).cuda()
 
     optimizer = torch.optim.Adam([w], lr=learning_rate)
@@ -37,8 +39,8 @@ def cw(model: nn.Module, images, labels, c=10.0, kappa=0, max_iter=1000, learnin
     prev = 1e10
 
     for step in range(max_iter):
-        if step % 10 == 0:
-            log.info(f'Step {step} out of {max_iter}')
+        if step % 50 == 0:
+            log.debug(f'Step {step} out of {max_iter}')
         a = 1 / 2 * (nn.Tanh()(w) + 1)
 
         loss1 = nn.MSELoss(reduction='sum')(a, images)
