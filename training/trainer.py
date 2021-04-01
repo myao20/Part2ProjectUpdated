3e35a9f53c36c183c72cbabc9cc4190a16e05495
myao1 <myao1135@gmail.com>
2021-04-01 15:51:27 +0100

    Change file names for training adversarial pgd model

--- training/trainer.py
+++ training/trainer.py
@@ -148,10 +148,10 @@ class Trainer:
             return val_loss, val_accuracy
 
     def write_logs_to_file(self) -> None:
-        write_list_to_file(self.train_loss, "advtrainlossfgsm.txt")
-        write_list_to_file(self.train_accuracy, "advtrainaccfgsm.txt")
-        write_list_to_file(self.val_loss, "advvallossfgsm.txt")
-        write_list_to_file(self.val_accuracy, "advvalaccfgsm.txt")
+        write_list_to_file(self.train_loss, "advtrainlosspgd.txt") # change shows on other changelist
+        write_list_to_file(self.train_accuracy, "advtrainaccpgd.txt")
+        write_list_to_file(self.val_loss, "advvallosspgd.txt")
+        write_list_to_file(self.val_accuracy, "advvalaccpgd.txt")
 
     def save_model_to_file(self, filename: str) -> None:
         save_model(self.model, filename)
